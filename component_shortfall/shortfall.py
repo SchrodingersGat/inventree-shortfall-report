a52a88b77705a69f276d58faaa638ef7c3c66541
Oliver Walters <oliver.henry.walters@gmail.com>
2026-02-26 06:27:56 +0000

    Rough loop

--- component_shortfall/shortfall.py
+++ component_shortfall/shortfall.py
@@ -1,4 +1,30 @@
-"""Functions for determining component shortfall."""
+"""Functions for determining component shortfall.
+
+Process Goals:
+
+For the provided set of parts, we want to detemine a complete list of all sub-components required to build the provided parts.
+
+- For each component we need to iterate down to the bottom of the BOM
+- Determine the required quantity of each component
+- Determine the available stock of each component
+- Determine the shortfall of each component (required - available)
+
+"""
+
+import structlog
+
+import part.models as part_models
+
+
+logger = structlog.get_logger('inventree.shortfall')
+
+
+def get_subassemblies(part):
+    """Return a list of subassemblies for the provided part."""
+
+    return part.get_bom_items(
+        include_virtual=False
+    ).filter(consumable=False)
 
 
 def calculate_shortfall(component_id_list: list[int], output_id: int):
@@ -9,6 +35,17 @@ def calculate_shortfall(component_id_list: list[int], output_id: int):
         output_id: The ID of the DataOutput (where to save the results)
     """
 
-    print("- calculate shortfall:")
-    print("-- part id:", component_id_list)
-    print("-- output id:", output_id)
+    # We keep a track of the required components (based on their ID)
+    # Each element in the dict will be a dict with the following keys:
+    # - part: The part object
+    # - required: The required quantity of the part
+    required_components : dict = {}
+
+    for component_id in component_id_list:
+        try:
+            part = part_models.Part.objects.get(pk=component_id)
+        except part_models.Part.DoesNotExist:
+            logger.warning(f"component_shortfall: Part with ID {component_id} does not exist - skipping")
+            continue
+
+    print("PROCESSING COMPLETE")
