273554f0fafe932f7e308c4fbd3dfa785eef1be8
Oliver Walters <oliver.henry.walters@gmail.com>
2026-03-16 00:59:41 +0000

    Fix for generated email:
    - Fix download link URL
    - Add part units
    - Improve email formatting

--- component_shortfall/shortfall.py
+++ component_shortfall/shortfall.py
@@ -24,7 +24,7 @@ import common.models as common_models
 import part.models as part_models
 
 
-logger = structlog.get_logger("inventree.shortfall")
+logger = structlog.get_logger("inventree.shortfall_report")
 
 
 def update_part_requirements(
@@ -300,7 +300,7 @@ def format_shortfall_report_html(
 
     # Add download link
     if output and output.output:
-        context_data["download_url"] = construct_absolute_url(output.output.url)
+        context_data["download_link"] = construct_absolute_url(output.output.url)
 
     # Add all the requirements entries
     requirements_list = []
